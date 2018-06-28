6597995f3f5a16cf564f16a84b42214f53577239
Unknown <musf_123@163.com>
2018-06-28 16:12:21 +0800

    规约

---
+++ GameExChange.Domain/Specifications/AnyExpressionSpecification.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace GameExChange.Domain.Specifications
+{
+    /// <summary>
+    /// 无条件表达式规约
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public sealed class AnyExpressionSpecification<T> : Specification<T>
+    {
+        public override Expression<Func<T, bool>> Expression
+        {
+            get { return o => true; }
+        }
+
+    }
+}
---
+++ GameExChange.Domain/Specifications/SpecExprExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace GameExChange.Domain.Specifications
+{
+    /// <summary>
+    /// 表达式树规约扩展方法
+    /// </summary>
+    public static class SpecExprExtensions
+    {
+
+        public static Expression<Func<T, bool>> Not<T>(this Expression<Func<T, bool>> expression)
+        {
+            var candidateExpression = expression.Parameters[0];
+            var body = Expression.Not(expression.Body);
+            return Expression.Lambda<Func<T, bool>>(body, candidateExpression);
+        }
+
+        public static Expression<Func<T, bool>> And<T>(this Expression<Func<T, bool>> one,
+            Expression<Func<T, bool>> another)
+        {
+            var candidateExpression = Expression.Parameter(typeof(T), "candidate");
+            var parameterReplaceHelper = new ParameterReplaceHelper(candidateExpression);
+
+            var left = parameterReplaceHelper.Replace(one.Body);
+            var right = parameterReplaceHelper.Replace(another.Body);
+
+            var body = Expression.And(left, right);
+
+            return Expression.Lambda<Func<T, bool>>(body, candidateExpression);
+        }
+
+        public static Expression<Func<T, bool>> Or<T>(this Expression<Func<T, bool>> one,
+            Expression<Func<T, bool>> another)
+        {
+            var candidateExpression = Expression.Parameter(typeof(T), "candidate");
+            var parameterReplaceHelper = new ParameterReplaceHelper(candidateExpression);
+
+            var left = parameterReplaceHelper.Replace(one.Body);
+            var right = parameterReplaceHelper.Replace(another.Body);
+
+            var body = Expression.Or(left, right);
+
+            return Expression.Lambda<Func<T, bool>>(body, candidateExpression);
+        }
+
+
+        
+    }
+}
