c733a2500b243024f30bf5aa93571a094eff655c
sars123321 <musf_123@163.com>
2018-07-02 16:37:52 +0800

    business fix

--- GameExChange.Business/Business/UserBusiness.cs
+++ GameExChange.Business/Business/UserBusiness.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using GameExChange.Business.Input.UserBusiness;
+using GameExChange.Business.Output.UserBusiness;
 
 namespace GameExChange.Business
 {
     public class UserBusiness : IBusiness.IUserBusiness
     {
+        public RegisterOutput Register(RegisterInput )
+        {
+            throw new NotImplementedException();
+        }
     }
 }
